f840d8ba95cf15c589f31067bed955df8bbc9630
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-21 00:14:55 +0600

    javascript style use in html file

---
+++ Modiul 1/js/style.js
@@ -0,0 +1,10 @@
+
+const sections = document.querySelectorAll('section');
+for( const section of sections){
+    section.style.border = '3px solid red';
+    section.style.marginBottom = ' 5px';
+    section.style.marginTop = '10px';
+    section.style.borderRadius ="10px";
+    section.style.paddingLeft = '15px';
+    section.style.backgroundColor ="lightgray";
+}
