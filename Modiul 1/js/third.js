f392ccb3fc11348b6c3bc895c580185286901576
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-21 16:39:19 +0600

    Module summary (DOM Review)

---
+++ Modiul 1/js/third.js
@@ -0,0 +1,15 @@
+
+const main = document.getElementById('main-container');
+
+const section = document.createElement('section');
+    section.innerHTML= `
+     <h1>section titel-4 by Maharob</h1>
+     <p>lorem is a djgd dbwej hied iwe</p>
+            <ul>
+                <li>List Item1</li>
+                <li>List Item2</li>
+                <li>List Item3</li>
+                <li>List Item4</li>
+            </ul>
+    `
+    main.appendChild(section);
