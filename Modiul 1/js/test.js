9c3ec1516980ca32be0cabda0accd134dfcea576
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-21 16:29:38 +0600

    Module summary (DOM Review)

--- Modiul 1/js/test.js
+++ Modiul 1/js/test.js
@@ -1,1 +1,11 @@
 console.log("Test js in console");
+
+const allLi = document.getElementsByTagName('li');
+console.log(allLi);
+
+const allTitel = document.getElementsByClassName('section-titel');
+console.log(allTitel);
+
+const secendSection = document.getElementById('secend-section');
+console.log(secendSection);
+secendSection.style.color ="violet";
