edd789750abd118a0fa2044d57ec5a143f8796bf
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-22 11:24:18 +0600

    Add onclick handler directly or via javascript

--- Modiul 2 DOM Event/js/event.js
+++ Modiul 2 DOM Event/js/event.js
@@ -4,3 +4,20 @@ console.log("This is separate file in DOM Event");
 function makeBlue(){
     document.body.style.backgroundColor = 'blue';
 }
+
+
+// Option 3: on click function()
+const makeAquaButton = document.getElementById('make-aqua');
+makeAquaButton.onclick = makeAqua;
+
+function makeAqua(){
+    document.body.style.backgroundColor = 'aqua';
+}
+
+
+
+        //   Option 4 another: on click function() 
+        const makePinkButton = document.getElementById('make-pink');
+        makePinkButton.onclick = function makePink(){
+            document.body.style.backgroundColor ='pink';
+        }
