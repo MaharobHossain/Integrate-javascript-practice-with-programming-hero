2d0e9bb2fa533b68d88264b9bd7e206de7fc16bc
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-04-17 09:48:37 +0600

    string number boolean type date cheaking

---
+++ Recap/validation.js
@@ -0,0 +1,10 @@
+
+const number = 25;
+
+// string number boolean type date cheaking
+if(typeof number === 'number'){
+    console.log('This is number');
+}
+else{
+    console.log('This is not number');
+}
